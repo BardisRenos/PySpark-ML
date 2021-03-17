f1839e4966b16cc253d8e4b3e2fb8a78121bab5d
Renos Bardis <renosbardis@gmail.com>
2021-03-17 01:36:44 +0100

    Creating the Training model by adding the Class

--- MLProject.py
+++ MLProject.py
@@ -5,89 +5,88 @@ from pyspark.sql import SparkSession
 findspark.init("/home/renos/Downloads/spark-3.0.1-bin-hadoop3.2")
 
 
-def relevant_data():
-    spark = SparkSession.builder.appName('ml-bank').getOrCreate()
+class NLPModel:
+    pass
 
-    df = spark.read.csv('/home/renos/Downloads/sf-crime/train.csv', header=True, inferSchema=True)
-    # print(df.columns)
-    # print(len(df.columns))
+    def relevant_data(self):
+        spark = SparkSession.builder.appName('ml-bank').getOrCreate()
 
-    data = df[['Category', 'Descript']]
-    # data.printSchema()
-    # data.show(5)
-    return data
+        df = spark.read.csv('/home/renos/Downloads/sf-crime/train.csv', header=True, inferSchema=True)
+        # print(df.columns)
+        # print(len(df.columns))
 
+        data = df[['Category', 'Descript']]
+        # data.printSchema()
+        # data.show(5)
+        return data
 
-def show_categories_descriptions():
-    from pyspark.sql.functions import col
+    def show_categories_descriptions(self):
+        from pyspark.sql.functions import col
 
-    data = relevant_data()
+        data = self.relevant_data()
 
-    data.groupBy("Category") \
-        .count() \
-        .orderBy(col("count").desc()) \
-        .show()
+        data.groupBy("Category") \
+            .count() \
+            .orderBy(col("count").desc()) \
+            .show()
 
-    data.groupBy("Descript") \
-        .count() \
-        .orderBy(col("count").desc()) \
-        .show()
+        data.groupBy("Descript") \
+            .count() \
+            .orderBy(col("count").desc()) \
+            .show()
 
+    def convert_the_label(self):
+        data = self.relevant_data()
+        from pyspark.ml import Pipeline
+        from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler
+        from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer
 
-def convert_the_label():
-    data = relevant_data()
-    from pyspark.ml import Pipeline
-    from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler
-    from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer
+        regexTokenizer = RegexTokenizer(inputCol="Descript", outputCol="words", pattern="\\W")
+        add_stopwords = ["http", "https", "amp", "rt", "t", "c", "the", "is", "a", "an", "and"]
+        stopwordsRemover = StopWordsRemover(inputCol="words", outputCol="filtered").setStopWords(add_stopwords)
 
-    regexTokenizer = RegexTokenizer(inputCol="Descript", outputCol="words", pattern="\\W")
-    add_stopwords = ["http", "https", "amp", "rt", "t", "c", "the", "is", "a", "an", "and"]
-    stopwordsRemover = StopWordsRemover(inputCol="words", outputCol="filtered").setStopWords(add_stopwords)
+        countVectors = CountVectorizer(inputCol="filtered", outputCol="features", vocabSize=10000, minDF=5)
+        label_string_Index = StringIndexer(inputCol="Category", outputCol="label")
+        pipeline = Pipeline(stages=[regexTokenizer, stopwordsRemover, countVectors, label_string_Index])
 
-    countVectors = CountVectorizer(inputCol="filtered", outputCol="features", vocabSize=10000, minDF=5)
-    label_string_Index = StringIndexer(inputCol="Category", outputCol="label")
-    pipeline = Pipeline(stages=[regexTokenizer, stopwordsRemover, countVectors, label_string_Index])
+        # Fit the pipeline to training documents.
+        pipeline_fit = pipeline.fit(data)
+        dataset = pipeline_fit.transform(data)
+        dataset.show()
 
-    # Fit the pipeline to training documents.
-    pipeline_fit = pipeline.fit(data)
-    dataset = pipeline_fit.transform(data)
-    dataset.show()
+        return dataset
 
-    return dataset
+    def split_data_for_training(self):
+        dataset = self.convert_the_label()
+        (trainingData, testData) = dataset.randomSplit([0.8, 0.2], seed=100)
+        print(f"Training Dataset length: {trainingData.count()} texts")
+        print(f"Test Dataset length: {testData.count()} texts")
 
+        return trainingData, testData
 
-def split_data_for_training():
-    dataset = convert_the_label()
-    (trainingData, testData) = dataset.randomSplit([0.8, 0.2], seed=100)
-    print(f"Training Dataset length: {trainingData.count()}")
-    print(f"Test Dataset length: {testData.count()}")
+    def model(self):
+        from pyspark.ml.classification import LogisticRegression
 
-    return trainingData, testData
+        trainingData, testData = self.split_data_for_training()
 
+        lr = LogisticRegression(maxIter=20, regParam=0.3, elasticNetParam=0)
+        lrModel = lr.fit(trainingData)
+        predictions = lrModel.transform(testData)
+        predictions.filter(predictions['prediction'] == 0) \
+            .select("Descript", "Category", "probability", "label", "prediction") \
+            .orderBy("probability", ascending=False) \
+            .show(n=10, truncate=30)
 
-def model():
-    from pyspark.ml.classification import LogisticRegression
+        return predictions
 
-    trainingData, testData = split_data_for_training()
+    def evaluate(self):
+        from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
-    lr = LogisticRegression(maxIter=20, regParam=0.3, elasticNetParam=0)
-    lrModel = lr.fit(trainingData)
-    predictions = lrModel.transform(testData)
-    predictions.filter(predictions['prediction'] == 0) \
-        .select("Descript", "Category", "probability", "label", "prediction") \
-        .orderBy("probability", ascending=False) \
-        .show(n=10, truncate=30)
-
-    return predictions
-
-
-def evaluate():
-    from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-
-    predictions = model()
-    evaluator = MulticlassClassificationEvaluator(predictionCol="prediction")
-    print(f"The Accuracy is:  {evaluator.evaluate(predictions)}")
+        predictions = self.model()
+        evaluator = MulticlassClassificationEvaluator(predictionCol="prediction")
+        print(f"The Accuracy is:  {format(evaluator.evaluate(predictions)*100, '.2f')} %")
 
 
 if __name__ == '__main__':
-    evaluate()
+    NlpModel = NLPModel()
+    NlpModel.evaluate()
